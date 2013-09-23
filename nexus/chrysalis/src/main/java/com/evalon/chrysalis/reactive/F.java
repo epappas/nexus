5c016902a95dedb9f9e2e8e8fd26364f74302250
Evangelos Pappas <papas.evagelos@gmail.com>
2013-09-23 11:43:59 +0300

    enhance Reactive

--- nexus/chrysalis/src/main/java/com/evalon/chrysalis/reactive/F.java
+++ nexus/chrysalis/src/main/java/com/evalon/chrysalis/reactive/F.java
@@ -3,10 +3,12 @@ package com.evalon.chrysalis.reactive;
 import com.evalon.chrysalis.concurrent.Conductor;
 import com.evalon.chrysalis.functional.Consumer;
 import com.evalon.chrysalis.functional.Function;
+import com.evalon.chrysalis.tuples.Pair;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 public class F<E> {
@@ -20,6 +22,11 @@ public class F<E> {
 		Collections.addAll(this.arrayList, es);
 	}
 
+	public F(Conductor conductor, ArrayList<E> list) {
+		this.conductor = conductor;
+		this.arrayList = list;
+	}
+
 	public F(Conductor conductor, Iterable<E> iterable) {
 		this.conductor = conductor;
 		this.arrayList = new ArrayList<E>();
@@ -30,7 +37,7 @@ public class F<E> {
 
 	public F(Conductor conductor, int from, int to) {
 		this.conductor = conductor;
-		this.arrayList = new java.util.ArrayList<E>();
+		this.arrayList = new ArrayList<E>();
 		for (int i = from; i < to; ++i) {
 			this.arrayList.add((E) new Integer(i));
 		}
@@ -72,6 +79,44 @@ public class F<E> {
 		return new F<E>(this.conductor, list);
 	}
 
+
+	public Conductor.Tone<ArrayList<E>> sort(final Function<Boolean, Pair<E, E>> function) {
+		final int size = this.arrayList.size();
+		// immutable Merge Sort
+		// new function in FP style ;)
+		final Function<ArrayList<E>, Pair<List<E>, List<E>>> merge = new Function<ArrayList<E>, Pair<List<E>, List<E>>>() {
+			@Override
+			public ArrayList<E> apply(Pair<List<E>, List<E>> pair) {
+				final ArrayList<E> list = new ArrayList<>();
+				ArrayList<E> x = (ArrayList<E>) pair.getLeft();
+				ArrayList<E> y = (ArrayList<E>) pair.getRight();
+
+				if (function.apply(new Pair<E, E>(x.get(0), y.get(0)))) {
+					list.add(x.get(0));
+					list.addAll(this.apply(new Pair<>(x.subList(1, x.size() - 1),
+							                                 y.subList(0, x.size() - 1))));
+					return list;
+				}
+				else {
+					list.add(y.get(0));
+					list.addAll(this.apply(new Pair<>(x.subList(0, x.size() - 1),
+							                                 y.subList(1, x.size() - 1))));
+					return list;
+				}
+			}
+		};
+
+		return conductor.conduct(new Callable<ArrayList<E>>() {
+			@Override
+			public ArrayList<E> call() throws Exception {
+				if (size > 1) {
+					return merge.apply(new Pair<>(takeFirst((int) size / 2).toList(), takeLast((int) (size / 2) + 1).toList()));
+				}
+				return arrayList;
+			}
+		});
+	}
+
 	public Conductor.Tone<ArrayList<E>> first(final Function<E, E> function) {
 		final ArrayList<E> list = new ArrayList<>();
 		return conductor.conduct(new Callable<ArrayList<E>>() {
@@ -92,6 +137,72 @@ public class F<E> {
 				return (ArrayList<E>) take(0, arrayList.size() - 1).toList();
 			}
 		});
+	}
+
+	public Conductor.Tone<ArrayList<E>> throttle(int t, final Function<ArrayList<E>, ArrayList<E>> function) {
+		final ArrayList<E> list = new ArrayList<>();
+		int size = arrayList.size();
+		int counter = t;
+		final F<E> first = takeFirst(t);
+
+		Conductor.Tone<java.util.ArrayList<E>> tone = conductor.conduct(new Callable<ArrayList<E>>() {
+			@Override
+			public ArrayList<E> call() throws Exception {
+				return function.apply((ArrayList) first.toList());
+			}
+		});
+
+		while ((counter += t) < size) {
+			final F<E> f = takeFirst(t);
+			tone = conductor.conductAfter(tone, new Callable<ArrayList<E>>() {
+				@Override
+				public ArrayList<E> call() throws Exception {
+					return function.apply((ArrayList) f.toList());
+				}
+			});
+		}
+
+		if (counter < size - 1) {
+			int tt = size - counter;
+			final F<E> f = takeFirst(tt);
+			tone = conductor.conductAfter(tone, new Callable<ArrayList<E>>() {
+				@Override
+				public ArrayList<E> call() throws Exception {
+					return function.apply((ArrayList) f.toList());
+				}
+			});
+		}
+
+		return tone;
+	}
+
+	public Conductor.Tone<ArrayList<E>> iterate(final Function<E, E> function) {
+		final ArrayList<E> list = new ArrayList<>();
+		final Iterator<E> it = arrayList.iterator();
+		final E first = it.next();
+
+		Conductor.Tone<ArrayList<E>> tone = conductor.conduct(new Callable<ArrayList<E>>() {
+			@Override
+			public ArrayList<E> call() throws Exception {
+				final E tmp = function.apply(first);
+				list.add(tmp);
+				return list;
+			}
+		});
+
+		while (it.hasNext()) {
+			final E tmpE = it.next();
+			tone = conductor.conductAfter(tone, new Callable<ArrayList<E>>() {
+				@Override
+				public ArrayList<E> call() throws Exception {
+					final E tmp = function.apply(tmpE);
+					list.add(tmp);
+					return list;
+				}
+			});
+		}
+
+		return tone;
 	}
 
 	public Conductor.Tone<ArrayList<E>> map(final Function<E, E> function) {
@@ -158,7 +269,7 @@ public class F<E> {
 		});
 	}
 
-	public java.util.List<E> toList() {
+	public List<E> toList() {
 		return this.arrayList;
 	}
 }
