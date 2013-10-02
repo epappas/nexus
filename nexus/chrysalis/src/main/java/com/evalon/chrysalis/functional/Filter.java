d2cee135b8496c907059c48875efaebcf948d505
Evangelos Pappas <papas.evagelos@gmail.com>
2013-10-02 15:49:33 +0300

    Interface of filter

---
+++ nexus/chrysalis/src/main/java/com/evalon/chrysalis/functional/Filter.java
@@ -0,0 +1,19 @@
+package com.evalon.chrysalis.functional;
+
+/**
+ * Interface to represent a boolean filter for messages
+ * 
+ * @param <T>
+ *            Type of message this filter is for
+ */
+public interface Filter<T>
+{
+	/**
+	 * Check to see whether the supplied message passes the filter
+	 * 
+	 * @param msg
+	 *            Message to check against filter
+	 * @return True upon success, false otherwise.
+	 */
+	boolean passes(T msg);
+}
